403f834d3ea697640c54836751212f4fa48a6d2e
Harsh <sharshvardhan759@gmail.com>
2024-06-17 20:38:10 +0530

    Recursion Day3

---
+++ Recursion/ReduceToZero.java
@@ -0,0 +1,21 @@
+//1342
+//https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
+
+public class ReduceToZero{
+    public static void main(String[] args) {
+        System.out.println(steps(41));
+    }
+    static int steps(int num){
+        return helper(num, 0);
+    }
+    static int helper(int num, int steps){
+        if(num==0){
+            return steps;
+        }
+        if(num%2 == 0){
+            return helper(num/2, steps + 1);
+        }
+
+        return helper(num-1, steps + 1);
+    }
+}
