d128c206ad148959dde8bce4065e39fe44bad0a6
Harsh <sharshvardhan759@gmail.com>
2024-06-23 18:46:06 +0530

    Day 4

--- Recursion/LinearSearchReg.java
+++ Recursion/LinearSearchReg.java
@@ -1,7 +1,11 @@
+
+import java.util.ArrayList;
+
 class LinearSearchReg{
     public static void main(String[] args) {
-        int[] arr = {6,7,8,4,9,3};
-        System.out.println(searchIndex(arr, 9, 0));
+        int[] arr = {6,7,8,4,9,3,9};
+        ArrayList<Integer> al = new ArrayList<>();
+        System.out.println(searchIndices(arr, 9, 0, al));
     }
 
     static boolean search(int[]arr, int target, int index){
@@ -19,5 +23,14 @@ class LinearSearchReg{
             return index;
         }
         return  searchIndex(arr, target, index + 1);
+    }
+    static ArrayList<Integer> searchIndices(int[]arr, int target, int index, ArrayList<Integer> al){
+        if(index == arr.length){
+            return al;
+        }
+        else if(arr[index]== target){
+            al.add(index);
+        }
+        return  searchIndices(arr, target, index + 1, al);
     }
 }
